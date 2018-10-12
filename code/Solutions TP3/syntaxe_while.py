2772f46c0280c6fc304861bb27cf9e1d8e37e4f2
Christophe Saint-Jean <christophe.saint-jean@univ-lr.fr>
2018-10-12 17:58:09 +0200

    Code TP3

---
+++ code/Solutions TP3/syntaxe_while.py
@@ -0,0 +1,65 @@
+x = 1
+while x <= 5:
+    print(x, end=' ')
+    x = x + 1
+
+
+
+print()
+print(('-'*20 + '\n')*2)
+
+x = 2
+while x <= 12:
+    print(x, end=' ')
+    x = x + 2
+    
+print()
+print('-'*20)
+
+x = 2
+while x <=12:
+    if x % 2 == 0:
+        print(x, end=' ')
+    x = x + 1
+    
+print()
+print(('-'*20 + '\n')*2)
+
+x = 3
+while x <= 21:
+    if x % 2 == 0:
+        print(x, end=' ')
+    x = x + 1
+    
+print()
+print('-'*20)
+
+x = 4
+while x <=21:
+    print(x, end=' ')
+    x = x + 2
+
+print()
+print(('-'*20 + '\n')*2)
+
+
+x = 130
+cpt = 0
+while cpt < 10:
+    if x % 5 == 0:
+        print(x, end=' ')
+        cpt = cpt + 1
+    x = x - 1
+    
+print()
+print('-'*20)
+
+x = 130
+cpt = 0
+while cpt < 10:
+    print(x, end=' ')
+    cpt = cpt + 1
+    x = x - 5
+    
+
+
