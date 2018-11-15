2547e55291e264542447c48e911a29b07cd1ae6c
Christophe Saint-Jean <christophe.saint-jean@univ-lr.fr>
2018-11-15 15:20:30 +0100

    mise en forme CM4

--- code/CM4_map.py
+++ code/CM4_map.py
@@ -1,12 +1,13 @@
 def f(x):
-    return x**2
+    return x ** 2
 
-def mon_map(L,fun):
+
+def mon_map(L, fun):
     res = []
     for l in L:
         res.append(fun(l))
     return res
 
-L = [1, 0, 3, 2, -3]
 
+L = [1, 0, 3, 2, -3]
 print(mon_map(L, f))
