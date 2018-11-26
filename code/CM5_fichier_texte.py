7a4d08694fe0ee38716a035f1be48e955b867cb6
Christophe Saint-Jean <christophe.saint-jean@univ-lr.fr>
2018-11-26 09:44:39 +0100

    Mise en ligne TP9

--- code/CM5_fichier_texte.py
+++ code/CM5_fichier_texte.py
@@ -1,26 +1,26 @@
-## Lecture
-
-# Lecture de tout en 1 instruction
-with open('alice.txt', mode='r') as f:
-    lignes = f.readlines()
-
-print(lignes[:9])
-
-# Lecture ligne par ligne
-
-with open('alice.txt', mode='r') as f:
-    while True:
-        ligne = f.readline()
-        if ligne != '':
-            print(ligne, end='')
-        else:
-            break   ## c'est la fin du fichier
-
-# Découpage d'une ligne
-une_ligne = lignes[8]
-print(une_ligne)
-print(une_ligne.split(' '))
-
+# ## Lecture
+#
+# # Lecture de tout en 1 instruction
+# with open('alice.txt', mode='r') as f:
+#     lignes = f.readlines()
+#
+# print(lignes[:9])
+#
+# # Lecture ligne par ligne
+#
+# with open('alice.txt', mode='r') as f:
+#     while True:
+#         ligne = f.readline()
+#         if ligne != '':
+#             print(ligne, end='')
+#         else:
+#             break   ## c'est la fin du fichier
+#
+# # Découpage d'une ligne
+# une_ligne = lignes[8]
+# print(une_ligne)
+# print(une_ligne.split(' '))
+#
 ## Ecriture
 L = [['Hauteur', 'Largeur', 'x0', 'y0'],
      [1, 20, 4, 0],
@@ -29,3 +29,4 @@ L = [['Hauteur', 'Largeur', 'x0', 'y0'],
 with open('L.txt', mode='w') as f:
     for l in L:
         print(l, file=f)
+        print(l)
---
+++ code/Solutions TP9/temperature.py
@@ -0,0 +1,8 @@
+def deCversF(tc):
+    return tc*9/5+32
+
+
+def deFversC(tf):
+    return (tf-32)*5/9
+
+print(deCversF(100))
