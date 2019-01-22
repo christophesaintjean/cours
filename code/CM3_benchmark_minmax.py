9e7884001af8d3892f46acdaa8bafcfa520264f3
Christophe Saint-Jean <christophe.saint-jean@univ-lr.fr>
2019-01-22 11:11:15 +0100

    CM3 - Typos 2

---
+++ code/CM3_benchmark_minmax.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from time import time
+from random import randint, seed
+
+from CM3_triinsert import triinsert
+from CM3_triselect import triselect
+
+seed(13)
+
+# a faire
+
+plt.fill_between(N, tmps_min, tmps_max)
