d2071e9c6666526801e8410da60ca31b94be6ee4
Christophe Saint-Jean <christophe.saint-jean@univ-lr.fr>
2019-08-14 16:26:20 +0200

    RAZ 2019

--- code/Solutions TP1/de_c_vers_f.py
+++
@@ -1,6 +0,0 @@
-# de C vers F
-tc = input("Saisissez la température en degrés Celsius : ")
-tc = float(tc)
-
-tf = tc*9/5+32
-print(tf, "degrés Farenheit")
--- code/Solutions TP1/de_f_vers_c.py
+++
@@ -1,6 +0,0 @@
-# de F vers C
-tf = input("Saisissez la température en degrés Farenheit : ")
-tf = float(tf)
-
-tc = (tf-32)*5/9
-print(tc, "degrés Celsius")
